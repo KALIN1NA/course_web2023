1f96d1a780a0672826ff825d6fabcaa216097dca
Anna3311 <anya.kalinina.3311@bk.ru>
2023-04-24 17:17:31 +0300

    Add Files

--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 const (
 	port = ":3000"
+	dbDriverName = "mysql"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/home", index)
-
-	// Реализуем отдачу статики
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
-	log.Println("Start server " + port)
-	err := http.ListenAndServe(port, mux)
+	db, err := openDB()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	dbx := sqlx.NewDb(db, dbDriverName)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/home", index(dbx))
+
+	mux.HandleFunc("/post", post)
+
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	log.Println("Start server " + port)
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openDB() (*sql.DB, error) {
+	return sql.Open(dbDriverName, "root:XFgw88spR@tcp(localhost:3306)/blog?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
 }
 
 
---
+++ data/sql/posts/posts/posts.sql
@@ -0,0 +1,151 @@
+INSERT INTO 
+    post   (
+        title, 
+        subtitle, 
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+) 
+VALUES (
+    'The Road Ahead', 
+    'The road ahead might be paved - it might not be.', 
+    'Mat Vogels', 
+    '/static/images/mat_vogels.png',
+    'September 25, 2015', 
+    'featured-post_northbright', 
+    1
+);
+
+INSERT INTO 
+    post    (
+        title, 
+        subtitle, 
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+) 
+VALUES (
+    'From Top Down', 
+    'Once a year.', 
+    'William Wong', 
+    '/static/images/william_wong.png',
+    'September 25, 2015', 'featured-post__from-top-down', 
+    1
+);
+
+INSERT INTO post    (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Still Standing Tall', 
+    'Life begins at the end of your comfort zone.', 
+    'William Wong', 
+    '/static/images/william_wong.png',
+    '9/25/2015', 
+    '/static/images/paraschutes.jpg', 
+    0
+);
+
+INSERT INTO post    (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Sunny Side Up', 
+    'No place is ever as bad as they tell you it''s going to be.', 
+    'Mat Vogels', 
+    '/static/images/mat_vogels.png',
+    '9/25/2015', 
+    '/static/images/bridge.jpg', 
+    0
+);
+INSERT INTO post    (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+)
+VALUES (
+    'Water Falls', 
+    'We travel not to escape life, but for life not to escape us.', 
+    'Mat Vogels', 
+    '/static/images/mat_vogels.png',
+    '9/25/2015', 
+    '/static/images/sunset.jpg', 
+    0
+);
+
+INSERT INTO post    (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Through the Mist', 
+    'Travel makes you see what a tiny place you occupy in the world.', 
+    'William Wong', 
+    '/static/images/william_wong.png',
+    '9/25/2015', 
+    '/static/images/through_the_mist.jpg', 
+    0
+);
+
+INSERT INTO post    (
+    title,
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Awaken Early', 
+    'Not all those who wander are lost.', 
+    'Mat Vogels', 
+    '/static/images/mat_vogels.png',
+    '9/25/2015', 
+    '/static/images/awaken_early.jpg', 
+    0
+);
+
+INSERT INTO post     (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Try It Always', 
+    'The world is a book, and those who do not travel read only one page.', 
+    'Mat Vogels', 
+    '/static/images/mat_vogels.png',
+    '9/25/2015', 
+    '/static/images/try_it_always.jpg', 
+    0
+);
