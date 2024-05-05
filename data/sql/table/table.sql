36fef47016947a460ca4a7c2e22d61eb02b68abc
KALIN1NA <142513421+KALIN1NA@users.noreply.github.com>
2024-05-05 20:13:52 +0300

    Update table.sql

--- data/sql/table/table.sql
+++ data/sql/table/table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE post
 (
     `post_id`         INT NOT NULL AUTO_INCREMENT,
-	`title`           VARCHAR(255) NOT NULL,
+    `title`           VARCHAR(255) NOT NULL,
     `subtitle`        VARCHAR(255) NOT NULL,
     `author`          VARCHAR(255) NOT NULL,
     `author_url`      VARCHAR(255) NOT NULL,
